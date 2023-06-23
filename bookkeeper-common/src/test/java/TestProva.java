db9c362a625103b4fd668dd40dfe260482e5fb6e
matteo <matteo.conti.977@gmail.com>
2023-06-23 21:42:30 +0200

    rimossi test e messa classe per provare jacoco e badua

---
+++ bookkeeper-common/src/test/java/TestProva.java
@@ -0,0 +1,15 @@
+import org.apache.bookkeeper.util.ProvaJacoco;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestProva {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+    @Test
+    public void whenEmptyString_thenAccept() {
+        ProvaJacoco palindromeTester = new ProvaJacoco();
+        assertTrue(palindromeTester.isPalindrome(""));
+    }
+}
