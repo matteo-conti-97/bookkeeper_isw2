9e33be3c37e789abbdcec65d421dd9452b2b5d5d
matteo <matteo.conti.977@gmail.com>
2023-08-21 00:28:49 +0200

    Iniziato a scrivere il test per il metodo addEntry di DefaultEntryLogger

--- bookkeeper-server/src/test/java/DefaultEntryLoggerAddEntryTest.java
+++ bookkeeper-server/src/test/java/DefaultEntryLoggerAddEntryTest.java
@@ -1,28 +1,30 @@
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import org.apache.bookkeeper.bookie.DefaultEntryLogger;
 import org.apache.bookkeeper.conf.ServerConfiguration;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
-@RunWith(value= Parameterized.class)
+@RunWith(Parameterized.class)
 public class DefaultEntryLoggerAddEntryTest {
     private ByteBuf entryBuff;
     private long ledgerId;
 
     @Parameters
-    public static Collection<Object> getTestParameters(){
-        return Arrays.asList(new Object[][]{
+    public static Collection<Object[]> getTestParameters(){
+        return Arrays.asList(new Object[][]{ //expected, ledgerId, entryBuff
+                {-1, Unpooled.buffer(1)},
+                {0, Unpooled.buffer(1)},
+                {0, null},
+                {0, Unpooled.buffer(0)}
         });
     }
 
-    public DefaultEntryLoggerAddEntryTest( long ledgerId, ByteBuf entryBuff){
-
+    public DefaultEntryLoggerAddEntryTest(long ledgerId, ByteBuf entryBuff){
         this.ledgerId = ledgerId;
         this.entryBuff = entryBuff;
     }
@@ -30,12 +32,8 @@ public class DefaultEntryLoggerAddEntryTest {
     @Test
     public void addEntryTest() {
         DefaultEntryLogger defaultEntryLogger = null;
-        try {
-            defaultEntryLogger = new DefaultEntryLogger(new ServerConfiguration());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Errore nella creazione di defaultEntryLogger");
-        }
+        System.out.println("LedgerId: " + ledgerId + " entryBuff: " + entryBuff);
+        Assert.assertEquals(0, 0);
     }
 
 }
