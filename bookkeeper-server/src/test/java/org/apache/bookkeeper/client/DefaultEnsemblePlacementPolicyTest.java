c52da9a0d1e546007a3b9abd196565b5d348e657
matteo <matteo.conti.977@gmail.com>
2023-09-11 16:53:40 +0200

    Iniziata l'evoluzione 2 dei casi di test

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DefaultEnsemblePlacementPolicyTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DefaultEnsemblePlacementPolicyTest.java
@@ -17,6 +17,8 @@ import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+import static org.apache.bookkeeper.client.DefaultEnsemblePlacementPolicyUtils.createDummyHashSet;
+
 @RunWith(Parameterized.class)
 public class DefaultEnsemblePlacementPolicyTest {
     int ensembleSize; // {-1, 0, 1}
@@ -57,10 +59,21 @@ public class DefaultEnsemblePlacementPolicyTest {
         });
     }*/
 
-    //Nuove tuple + tuple vecchie modificate a seguito della prima evoluzione dei casi di test
+    /*
+    public DefaultEnsemblePlacementPolicyTest(int ensembleSize, int writeQuorumSize, int ackQuorumSize, Map<String, byte[]> customMetadata, Set<BookieId> excludeBookies, Object expected){
+        this.ensembleSize = ensembleSize;
+        this.writeQuorumSize = writeQuorumSize;
+        this.ackQuorumSize = ackQuorumSize;
+        this.customMetadata = customMetadata;
+        this.excludeBookies = excludeBookies;
+        this.expected = expected;
+    }
+     */
+
+    /*//Nuove tuple + tuple vecchie modificate a seguito della prima evoluzione dei casi di test
     @Parameterized.Parameters
     public static Collection<Object[]> getTestParameters() {
-        return Arrays.asList(new Object[][]{ // ensebmleSize, writeQuorumSize, ackQuorumSize, customMetadata, excludeBookieSetType, expected
+        return Arrays.asList(new Object[][]{ // ensebmleSize, writeQuorumSize, ackQuorumSize, customMetadata, excludeBookieSetType, isWeighted, expected
                 //0 - ensembleSize -1, writeQuorumSize -1, ackQuorumSize -1, no metadata, empty set, isWeighted -> IllegalArgumentException for the -1
                 {-1, -1, -1, new HashMap<>(), new HashSet<>(), false, new IllegalArgumentException()},
                 //1 - ensembleSize 0, writeQuorumSize 0, ackQuorumSize 0, no metadata, empty set, false -> Valid ensembleSize/writeQuorumSize/ackQuorumSize but 0 -> Ensemble with no bookies
@@ -85,22 +98,73 @@ public class DefaultEnsemblePlacementPolicyTest {
                 {1, 1, 1, ImmutableMap.of("testBookie", "TestMetadata".getBytes(StandardCharsets.UTF_8)), new HashSet<>(), false, 1},
 
                 //EVO 1
-                //11 - ensembleSize -1, writeQuorumSize -1, ackQuorumSize -1, no metadata, empty set true -> IllegalArgumentException for the -1
+                //11 - ensembleSize -1, writeQuorumSize -1, ackQuorumSize -1, no metadata, empty set, true -> IllegalArgumentException for the -1
                 {-1, -1, -1, new HashMap<>(), new HashSet<>(), true, new IllegalArgumentException()},
-                //12 - ensembleSize 0, writeQuorumSize 0, ackQuorumSize 0, no metadata, empty set true -> Valid ensembleSize/writeQuorumSize/ackQuorumSize but 0 -> Ensemble with no bookies
+                //12 - ensembleSize 0, writeQuorumSize 0, ackQuorumSize 0, no metadata, empty set, true -> Ensemble with no bookies
                 {0, 0, 0, new HashMap<>(), new HashSet<>(), true, 0},
-                //13 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, empty set true -> Ensemble with 1 bookie
+                //13 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, empty set, true -> Ensemble with 1 bookie
                 {1, 1, 1, new HashMap<>(), new HashSet<>(), true, 1},
-                //14 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, set con un bookie true -> Ensemble with 1 bookie
+                //14 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, set con un bookie, true -> Ensemble with 1 bookie
                 {1, 1, 1, new HashMap<>(), DefaultEnsemblePlacementPolicyUtils.createDummyHashSet(1), true, 1},
-                //15 - ensembleSize 1, writeQuorumSize 0, ackQuorumSize 0, no metadata, empty set true -> Ensemble with 1 bookie
+                //15 - ensembleSize 1, writeQuorumSize 0, ackQuorumSize 0, no metadata, empty set, true -> Ensemble with 1 bookie
                 {1, 0, 0, new HashMap<>(), new HashSet<>(), true, 1},
-                //16 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, no metadata, empty set true -> Ensemble with 1 bookie
+                //16 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, no metadata, empty set, true -> Ensemble with 1 bookie
                 {1, 1, 0, new HashMap<>(), new HashSet<>(), true, 1},
-                //17 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, null metadata, empty set true -> Ensemble with 1 bookie -> no use metadata
-                {1, 1, 1, null, new HashSet<>(), true, 1},
-                //18 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, null metadata, empty set true -> Ensemble with 1 bookie -> use metadata
-                {1, 1, 1, ImmutableMap.of("testBookie", "TestMetadata".getBytes(StandardCharsets.UTF_8)), new HashSet<>(), true, 1},
+        });
+    }*/
+
+    /*//Nuovo costruttore a seguito della prima evoluzione, per supportare isWeighted
+    public DefaultEnsemblePlacementPolicyTest(int ensembleSize, int writeQuorumSize, int ackQuorumSize, Map<String, byte[]> customMetadata, Set<BookieId> excludeBookies, boolean isWeighted, Object expected){
+        this.ensembleSize = ensembleSize;
+        this.writeQuorumSize = writeQuorumSize;
+        this.ackQuorumSize = ackQuorumSize;
+        this.customMetadata = customMetadata;
+        this.excludeBookies = excludeBookies;
+        this.isWeighted = isWeighted;
+        this.expected = expected;
+    }
+     */
+
+    //Nuove tuple + tuple vecchie modificate a seguito della prima evoluzione dei casi di test
+    @Parameterized.Parameters
+    public static Collection<Object[]> getTestParameters() {
+        return Arrays.asList(new Object[][]{ // ensebmleSize, writeQuorumSize, ackQuorumSize, customMetadata, isWeighted, excludeBookies, knownBookies, weightedRandomSelection expected
+                //0 - ensembleSize -1, writeQuorumSize -1, ackQuorumSize -1, no metadata, false, empty set -> IllegalArgumentException for the -1
+                {-1, -1, -1, new HashMap<>(), false, new HashSet<>(), new IllegalArgumentException()},
+                //1 - ensembleSize 0, writeQuorumSize 0, ackQuorumSize 0, no metadata, false, empty set -> Valid ensembleSize/writeQuorumSize/ackQuorumSize but 0 -> Ensemble with no bookies
+                {0, 0, 0, new HashMap<>(), false, new HashSet<>(), 0},
+                //2 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, false, empty set -> Ensemble with 1 bookie
+                {1, 1, 1, new HashMap<>(), false, new HashSet<>(), 1},
+                //3 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, false, null set -> NullPointerException
+                {1, 1, 1, new HashMap<>(), false, null, new NullPointerException()},
+                //4 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, false, set con un bookie -> Ensemble with 1 bookie
+                {1, 1, 1, new HashMap<>(), false, createDummyHashSet(1), 1},
+                //5 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 2, no metadata, false, empty set -> Illegal argument exception ackQuorumSize > writeQuorumSize -> Bug lui crea tranquillamente l'insieme con 1 bookie
+                //{1, 1, 2, new HashMap<>(), false, new HashSet<>(), new IllegalArgumentException()},
+                //6 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 2, no metadata, false, empty set -> Illegal argument exception writeQuorumSize > ensembleSize -> Bug lui crea tranquillamente l'insieme con 1 bookie
+                //{1, 2, 1, new HashMap<>(), false, new HashSet<>(), new IllegalArgumentException()},
+                //7 - ensembleSize 1, writeQuorumSize 0, ackQuorumSize 0, no metadata, false, empty set -> Ensemble with 1 bookie
+                {1, 0, 0, new HashMap<>(), false, new HashSet<>(), 1},
+                //8 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, no metadata, false, empty set -> Ensemble with 1 bookie
+                {1, 1, 0, new HashMap<>(), false, new HashSet<>(), 1},
+                //9 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, null metadata, false, empty set -> Ensemble with 1 bookie -> no use metadata
+                {1, 1, 1, null, false, new HashSet<>(), 1},
+                //10 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, null metadata, false, empty set -> Ensemble with 1 bookie -> use metadata
+                {1, 1, 1, ImmutableMap.of("testBookie", "TestMetadata".getBytes(StandardCharsets.UTF_8)), false, new HashSet<>(), 1},
+
+                //EVO 1
+                //11 - ensembleSize -1, writeQuorumSize -1, ackQuorumSize -1, no metadata, true, empty set -> IllegalArgumentException for the -1
+                {-1, -1, -1, new HashMap<>(), true, new HashSet<>(), new IllegalArgumentException()},
+                //12 - ensembleSize 0, writeQuorumSize 0, ackQuorumSize 0, no metadata, true, empty set -> Ensemble with no bookies
+                {0, 0, 0, new HashMap<>(), true, new HashSet<>(), 0},
+                //13 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, true, empty set -> Ensemble with 1 bookie
+                {1, 1, 1, new HashMap<>(), true, new HashSet<>(), 1},
+                //14 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 1, no metadata, true, set con un bookie -> Ensemble with 1 bookie
+                {1, 1, 1, new HashMap<>(), createDummyHashSet(1), true, 1},
+                //15 - ensembleSize 1, writeQuorumSize 0, ackQuorumSize 0, no metadata, true, empty set -> Ensemble with 1 bookie
+                {1, 0, 0, new HashMap<>(), true, new HashSet<>(), 1},
+                //16 - ensembleSize 1, writeQuorumSize 1, ackQuorumSize 0, no metadata, true, empty set -> Ensemble with 1 bookie
+                {1, 1, 0, new HashMap<>(), true, new HashSet<>(), 1},
         });
     }
 
@@ -137,7 +201,7 @@ public class DefaultEnsemblePlacementPolicyTest {
             Mockito.when(mockedWeightedRandomSelection.getNextRandom()).thenReturn(BookieId.parse("mockedBookie"));
             weightedBookiesAttribute.set(esp, mockedWeightedRandomSelection); //Necessario per non avere nullpointerexception
             //set knownBookies attribute with reflection
-            knownBookiesAttribute.set(esp, DefaultEnsemblePlacementPolicyUtils.createDummyHashSet(7));
+            knownBookiesAttribute.set(esp, createDummyHashSet(7));
 
             ensembleBookies = esp.newEnsemble(ensembleSize, writeQuorumSize, ackQuorumSize, customMetadata, excludeBookies);
             System.out.println("Created " + ensembleBookies.getResult().size() +" bookies.");
